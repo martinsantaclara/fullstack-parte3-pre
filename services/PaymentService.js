b1bca626e40bb2f304023406be6e40ece511f9f4
Martin <martinsantaclara@gmail.com>
2022-06-22 17:26:16 -0300

    uibuild

--- services/PaymentService.js
+++ services/PaymentService.js
@@ -1,29 +1,29 @@
 const axios = require('axios')
 
 class PaymentService {
-    async createPayment(body) {
+    async createPayment() {
         const url = 'https://api.mercadopago.com/checkout/preferences'
 
         // console.log(body)
 
-        // const body = {
-        //   payer_email: "test_user_24559756@testuser.com",
-        //   items: [
-        //     {
-        //       title: "Dummy Title",
-        //       description: "Dummy description",
-        //       picture_url: "http://www.myapp.com/myimage.jpg",
-        //       category_id: "category123",
-        //       quantity: 2,
-        //       unit_price: 20
-        //     }
-        //   ],
-        //   back_urls: {
-        //     failure: "https://google.com.ar",
-        //     pending: "https://google.com.ar",
-        //     success: "https://google.com.ar"
-        //   }
-        // };
+        const body = {
+            payer_email: 'test_user_24559756@testuser.com',
+            items: [
+                {
+                    title: 'Dummy Title',
+                    description: 'Dummy description',
+                    picture_url: 'http://www.myapp.com/myimage.jpg',
+                    category_id: 'category123',
+                    quantity: 2,
+                    unit_price: 20
+                }
+            ],
+            back_urls: {
+                failure: 'https://google.com.ar',
+                pending: 'https://google.com.ar',
+                success: 'https://google.com.ar'
+            }
+        }
 
         const payment = await axios.post(url, body, {
             headers: {
