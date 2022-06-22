9247d6f0990921e1a48b844a12da450c3b8e5491
Martin <martinsantaclara@gmail.com>
2022-06-22 10:06:17 -0300

    uibuild

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ app.post('/rick', async (req, res) => {
                 Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
             }
         })
-        return res.json(data)
+        res.status(200).json(data)
     } catch (err) {
         res.status(500).json({ message: 'Server error' })
     }
